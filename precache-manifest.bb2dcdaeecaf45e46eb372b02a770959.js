241b36ae2b8077dd4933969591e69410d986789d
Inyama, Uchechukwu <depoulet@yahoo.com>
2019-01-24 11:26:31 +0100

    Updates

---
+++ precache-manifest.bb2dcdaeecaf45e46eb372b02a770959.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0c771f41d5d1a0b6cc82",
+    "url": "/static/js/main.0c771f41.chunk.js"
+  },
+  {
+    "revision": "3f50b919bde25ec96d86",
+    "url": "/static/js/1.3f50b919.chunk.js"
+  },
+  {
+    "revision": "7ef77cc6ba902a0b6f03b3f51d706cf6",
+    "url": "/index.html"
+  }
+];
